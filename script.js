ffc92309c5d473ff60b86a3687dfe45b48784456
ariana <arianaegiziano@gmail.com>
2020-11-03 12:23:33 +1100

    edits to script.js and HTML

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function onLoad(){
+function onLoad() {
     const btn = document.querySelector('#navbutton')
     const nav = document.querySelector('.navigation')
-
-    btn.addEventListener( 'click' , function() {
-        if( nav.classList.contains('open') ) {
-            nav.classList.remove('open')
-          }
-          else {
-            nav.classList.add('open')
-          }
+  
+    btn.addEventListener('click', function() {
+      if( nav.classList.contains('open') ) {
+        nav.classList.remove('open')
+      }
+      else {
+        nav.classList.add('open')
+      }
     } )
- }
+  }
+  
+  window.addEventListener( 'load' , onLoad )
