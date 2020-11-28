38470b3de21c627665e4d3db85148bd30de580c2
ariana <arianaegiziano@gmail.com>
2020-11-28 11:31:00 +1100

    fixed validation errors

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function onLoad() {
-    const btn = document.querySelector('#navbutton')
-    const nav = document.querySelector('.navigation')
+    const btn = document.querySelector("#navbutton")
+    const nav = document.querySelector("navigation")
   
-    btn.addEventListener('click', function() {
-      if( nav.classList.contains('open') ) {
-        nav.classList.remove('open')
+    btn.addEventListener("click", function() {
+      if( nav.classList.contains("open") ) {
+        nav.classList.remove("open")
       }
       else {
-        nav.classList.add('open')
+        nav.classList.add("open")
       }
     } )
     //slideshow
-    const slideshow = document.querySelector('#slideshow')
+    const slideshow = document.querySelector("#slideshow")
     const slides = new slides (slideshow, {})
   }
   
-  window.addEventListener( 'load' , onLoad )
+  window.addEventListener( "load" , onLoad )
