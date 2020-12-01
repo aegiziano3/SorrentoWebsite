62dbf2ae4ea74706e0ead39f368a7ab130e6c6a6
ariana <arianaegiziano@gmail.com>
2020-12-01 11:11:37 +1100

    romeved white space script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function onLoad() {
-    const btn = document.querySelector("#navbutton")
-    const nav = document.querySelector("navigation")
-  
-    btn.addEventListener("click", function() {
-      if( nav.classList.contains("open") ) {
-        nav.classList.remove("open")
-      }
-      else {
-        nav.classList.add("open")
-      }
-    } )
-    //slideshow
-    const slideshow = document.querySelector("#slideshow")
-    const slides = new slides (slideshow, {})
-  }
-  
-  window.addEventListener( "load" , onLoad )
+  const btn = document.querySelector("#navbutton")
+  const nav = document.querySelector("navigation")
+  btn.addEventListener("click", function() {
+    if( nav.classList.contains("open") ) {
+      nav.classList.remove("open")
+    }
+    else {
+      nav.classList.add("open")
+    }
+  } )
+  //slideshow
+  const slideshow = document.querySelector("#slideshow")
+  const slides = new slides (slideshow, {})
+}
+window.addEventListener( "load" , onLoad )
